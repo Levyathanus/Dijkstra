a7c87666254ef8cd58a2438120dc76fef0a88a4c
Levyathanus <michelezenoni1@gmail.com>
2020-08-30 17:17:12 +0200

    create graph from adjcency matrix

--- graph.py
+++ graph.py
@@ -65,6 +65,26 @@ class Graph:
     def __str__(self):
         return self.name
 
+    @property
+    def root(self):
+        return self.__root
+
+    @property
+    def node_number(self):
+        return self.__node_number
+
+    @property
+    def edge_number(self):
+        return self.__edge_number
+
+    @root.setter
+    def set_root(self, node):
+        if node in self.nodes:
+            self.__root = node
+            return True
+        else:
+            return False
+
     def adjacency_matrix(self):
         matrix = [[0 for i in range(self.__node_number)] for j in range(self.__node_number)]
         
@@ -81,32 +101,32 @@ class Graph:
                             matrix[j][i] = 1
 
         return matrix
+
+    def weights_matrix(self):
+        if not self.is_weighted():
+            return None
+        matrix = [[0 for i in range(self.__node_number)] for j in range(self.__node_number)]
+
+        for i in range(self.__node_number):
+            for j in range(self.__node_number):
+                for k in range(self.__edge_number):
+                    if self.edges[k].origin == self.nodes[i] and self.edges[k].destination == self.nodes[j]:
+                        if self.edges[k].direction == 1:
+                            matrix[i][j] = self.edges[k].weight
+                        elif self.edges[k].direction == 0:
+                            matrix[i][j] = self.edges[k].weight
+                            matrix[j][i] = self.edges[k].weight
+                        elif self.edges[k].direction == -1:
+                            matrix[j][i] = self.edges[k].weight
+    
+        return matrix
+        
                 
     def summarize(self):
         print("Graph name: " + self.name)
         print("Number of nodes: " + str(self.__node_number))
         print("Number of edges: " + str(self.__edge_number))
         print("Current root node: " + str(self.__root))
-    
-    @property
-    def root(self):
-        return self.__root
-
-    @property
-    def node_number(self):
-        return self.__node_number
-
-    @property
-    def edge_number(self):
-        return self.__edge_number
-    
-    @root.setter
-    def set_root(self, node):
-        if node in self.nodes:
-            self.__root = node
-            return True
-        else:
-            return False
 
     def create_undirected_edges(self, weights = []):
         if len(weights) < self.__node_number - 1:
@@ -144,15 +164,54 @@ class Graph:
             return False
         check = True
         for edge in self.edges:
-            if edge.weight == float('inf') or edge.weight == 0:
+            if edge.weight == float('inf') or edge.weight <= 0:
                 check = False
                 break
         return check
 
-    #TODO: def is_connected(self):
-
 def print_matrix(matrix):
     print('\n'.join([' '.join(['{}'.format(item) for item in row]) for row in matrix]))
+
+def create_graph(name = "Graph", adj_matrix = [], w_matrix = []):
+    n = len(adj_matrix)
+    if n == 0 or n != len(adj_matrix[0]):
+        print("Adjcency matrix format is not valid!")
+        return None
+    p = len(w_matrix)
+    nodes = []
+    edges = []
+    for i in range(n):
+            V = Node("Node_" + str(i))
+            nodes.append(V)
+    directed = True
+    for i in range(n):
+        for j in range(n):
+            if adj_matrix[i][j] != adj_matrix[j][i]:
+                directed = False
+    if p == 0 or p != n:
+        print("Weight matrix format is not valid, ignoring it...")
+        for i in range(n):
+            for j in range(n):
+                if adj_matrix[i][j] == 1:
+                    if not directed:
+                        E = Edge(nodes[i], nodes[j], weight = 0, direction = 0)
+                    else:
+                        E = Edge(nodes[i], nodes[j], weight = 0, direction = 0)
+                        adj_matrix[j][i] = 0
+                    edges.append(E)
+    else:
+        for i in range(n):
+            for j in range(n):
+                if adj_matrix[i][j] == 1:
+                    if not directed:
+                        E = Edge(nodes[i], nodes[j], weight = w_matrix[i][j], direction = 0)
+                    else:
+                        E = Edge(nodes[i], nodes[j], weight = w_matrix[i][j], direction = 1)
+                        adj_matrix[j][i] = 0
+                    edges.append(E)
+
+    return Graph(name, nodes, edges)
+
 
 if __name__ == "__main__":
     a = Node("Test", Test = True)
@@ -169,4 +228,6 @@ if __name__ == "__main__":
     print(G.edges[0])
     print_matrix(G.adjacency_matrix())
     print(G.is_weighted())
+    P = G.weights_matrix()
+    print(P)
     print("Done!")
